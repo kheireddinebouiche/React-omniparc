c077c108d61fbe1540de5e048d64beee224bd302
kheireddinebouiche <kheirddinebouiche@gmail.com>
2025-04-08 19:08:04 +0100

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
         manualChunks: {
           vendor: ['react', 'react-dom', 'react-router-dom'],
           firebase: ['firebase/app', 'firebase/auth', 'firebase/firestore'],
-          ui: ['@mui/material', '@mui/icons-material', '@emotion/react', '@emotion/styled'],
+          ui: ['@chakra-ui/react', '@emotion/react', '@emotion/styled'],
         },
       },
     },
